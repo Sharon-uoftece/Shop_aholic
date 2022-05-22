e008100a6b855a3dfbd0530a937a0222abfd42a4
Siyuan sharon Li <sharonsiyuan.li@mail.utoronto.ca>
2022-05-21 23:25:57 -0400

    added three products

---
+++ frontend/src/data.js
@@ -0,0 +1,41 @@
+const data = {
+    products:[
+        {
+            name: 'Maison Margiela Glam Slam Shoulder Bag',
+            slug: 'maison-margiela-glam-slam-shoulder-bag',
+            category: 'Handbag',
+            image: '/images/glamSlam.jpg',
+            price: 1910,
+            countInStock: 10,
+            brand: 'Maison Margiela',
+            rating: 4.8,
+            numReviews: 12,
+            description: 'Padded grained leather shoulder bag in black. Detachable and adjustable logo-woven webbing shoulder strap with cinch and hook-eye fastenings. Leather logo patch featuring signature white stitching at face. Zip closure. Zippered pocket featuring textile logo patch at interior. Tonal textile lining. Tonal hardware. '
+        },
+        {
+            name: 'Marni Glam Soft Trunk Bag',
+            slug: 'marni-glam-soft-trunk-bag',
+            category: 'Handbag',
+            image: '/images/softTrunk.jpg',
+            price: 1885,
+            countInStock: 19,
+            brand: 'Marni',
+            rating: 4.6,
+            numReviews: 5,
+            description: 'Tumbled calfskin messenger bag in orange.'
+        },
+        {
+            name: 'Jil Sander Flat Shopper Tote',
+            slug: 'jil-sander-flat-shopper-tote',
+            category: 'Tote',
+            image: '/images/flatTote.jpg',
+            price: 710,
+            countInStock: 9,
+            brand: 'Jil Sander',
+            rating: 4.2,
+            numReviews: 4,
+            description: 'Cotton and linen-blend canvas tote in off-white. Twin leather carry handles at top. '
+        },
+    ],
+};
+export default data;
