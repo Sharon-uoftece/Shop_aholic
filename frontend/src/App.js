0f7baf4699187f32580b15d5e65c52437bd49f53
Siyuan sharon Li <sharonsiyuan.li@mail.utoronto.ca>
2022-05-24 14:49:42 -0400

    backend code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,22 @@
-import data from "./data";
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import HomeScreen from "./HomeScreen";
+import ProductScreen from "./ProductScreen";
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Shopaholic</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p><strong>${product.price}</strong></p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-            ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <a href="/">Shopaholic</a>
+        </header>
+        <main>
+          <Routes> 
+            <Route path="/product/:slug" element={<ProductScreen/>} />
+            <Route path="/" element={<HomeScreen/>}/>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
